1bb608f8663a7e28f4dd57844d2270dfeb78b14c
Joel Lanciaux <joel@amberengine.com>
2017-10-27 13:48:30 -0400

    More form stories.

--- stories/components/form-stories.js
+++ stories/components/form-stories.js
@@ -3,24 +3,41 @@ import { storiesOf } from '@storybook/react';
 
 import { Form, FormGroup, Fields, Submit } from '../../src/components/Form';
 
-const dropdownOptions = [
-  { value: 'AL', label: 'Alabama' },
-  { value: 'AK', label: 'Alaska' },
-  { value: 'AS', label: 'American Samoa' },
-  { value: 'AZ', label: 'Arizona' },
-  { value: 'AR', label: 'Arkansas' },
-  { value: 'CA', label: 'California' },
-  { value: 'CO', label: 'Colorado' },
-  { value: 'CT', label: 'Connecticut' },
-  { value: 'DE', label: 'Delaware' },
-];
+const onFormSubmit = data => console.log('submitted!', data);
 
 storiesOf('Form', module)
-  .add('renders dropdown', () => {
+  .add('renders input', () => {
     return (
-      <Form
-        onSubmit={data => console.log('submitted!', data)}
-      >
+      <Form onSubmit={onFormSubmit}>
+        <FormGroup
+          title="Template"
+          name="template"
+          className="form-control"
+          FieldComponent={Fields.ValidatedInputField}
+        />
+        <Submit
+          type="submit"
+        >
+          EXPORT
+        </Submit>
+      </Form>
+    );
+  })
+  .add('renders dropdown', () => {
+    const dropdownOptions = [
+      { value: 'AL', label: 'Alabama' },
+      { value: 'AK', label: 'Alaska' },
+      { value: 'AS', label: 'American Samoa' },
+      { value: 'AZ', label: 'Arizona' },
+      { value: 'AR', label: 'Arkansas' },
+      { value: 'CA', label: 'California' },
+      { value: 'CO', label: 'Colorado' },
+      { value: 'CT', label: 'Connecticut' },
+      { value: 'DE', label: 'Delaware' },
+    ];
+
+    return (
+      <Form onSubmit={onFormSubmit}>
         <FormGroup
           title="Template"
           name="template"
