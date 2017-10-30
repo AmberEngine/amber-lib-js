3d2d52cc8e51a5544d0dffd3854c607873e0c994
Andrew Joye <andrew@amberengine.com>
2017-10-30 16:57:11 -0400

    sharing is caring

---
+++ src/components/SelectModal/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import Form, FormGroup, Fields, Submit from '../Form';
+import { required } from '../Form/Validation';
+
+import stylesheet from './SelectModal.scss';
+
+export default class SelectModal extends Component {
+  render() {
+    const {
+      onSubmit,
+      options,
+      toggleModal,
+      visible,
+      label,
+      title,
+      name
+    } = this.props;
+
+    return (
+      <Modal
+        isOpen={visible}
+        onClose={toggleModal}
+        label={label}
+        className={stylesheet.modalContainer}
+      >
+        <Form
+          onSubmit={onSubmit}
+          className={stylesheet.formWrapper}
+        >
+          <FormGroup
+            title={title}
+            name={name}
+            className="form-control"
+            validate={[required]}
+            FieldComponent={Fields.ValidatedDropDownField}
+            options={options}
+          />
+          <Submit
+            type="submit"
+            className={stylesheet.submitButton}
+          >
+            SUBMIT
+          </Submit>
+        </Form>
+      </Modal>
+    );
+  }
+}
