1a524dda89f9b63c7c10525f8a78f63fbec8d846
Joel Lanciaux <joel@amberengine.com>
2017-11-08 17:19:37 -0500

    .Added lerna, building.

--- workspaces/unformed/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * as Enhancers from './Enhancers';
-export * as Fields from './Fields';
-export * as Validation from './Validation';
-export Form from './Form';
-export FormGroup from './FormGroup';
--- workspaces/unformed/src/index.js
+++ workspaces/unformed/src/index.js
@@ -1,4 +1,9 @@
-export components from './components';
+export * as Enhancers from './components/Enhancers';
+export * as Fields from './components/Fields';
+export * as Validation from './components/Validation';
+export Form from './components/Form';
+export FormGroup from './components/FormGroup';
+export utils from './utils';
 
 import * as self from './index';
 export default self;
