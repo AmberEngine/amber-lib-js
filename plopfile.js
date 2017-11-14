187b433afdae27d1b55f34cf4d8f240e361feb05
Joel Lanciaux <joel@amberengine.com>
2017-11-14 13:43:08 -0500

    Removed amber-generator.

--- plopfile.js
+++ plopfile.js
@@ -9,12 +9,12 @@ module.exports = (plop) => {
     actions: [{
       type: 'addMany',
       templateFiles: 'templates/library/**',
-      destination: '../{{name}}/',
+      destination: 'workspaces/{{name}}/',
       base: 'templates/library'
     }, {
       type: 'add',
       templateFile: 'templates/library/.gitignore',
-      path: '../{{name}}/.gitignore'
+      path: 'workspaces/{{name}}/.gitignore'
     }],
   });
   plop.setGenerator('component-library', {
@@ -27,12 +27,12 @@ module.exports = (plop) => {
     actions: [{
       type: 'addMany',
       templateFiles: 'templates/component-lib/**',
-      destination: '../{{name}}/',
+      destination: 'workspaces/{{name}}/',
       base: 'templates/component-lib'
     }, {
       type: 'add',
       templateFile: 'templates/component-lib/.gitignore',
-      path: '../{{name}}/.gitignore'
+      path: 'workspaces/{{name}}/.gitignore'
     }],
   });
 };
