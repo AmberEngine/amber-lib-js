3b13ea4b3dc01485eecd39a58a1d931b96f8882b
Joel Lanciaux <joel@amberengine.com>
2017-11-20 12:03:05 -0500

    Fix amber-content export.

--- workspaces/amber-lib/src/index.js
+++ workspaces/amber-lib/src/index.js
@@ -2,18 +2,18 @@ import { components, utils } from '@amber-engine/amber-components';
 import Form from 'unformed';
 import FormFields from 'unformed-fields';
 
-import { components as ContentComponents } from '@amber-engine/amber-content';
+import { Icon } from '@amber-engine/amber-content';
 
 export default {
   components: {
     ...components,
-    ...ContentComponents,
+    Icon,
     FormComponents: {
       ...Form,
       Fields: {
         ...Form.Fields,
         ...FormFields,
-      }
+      },
     },
   },
   utils,
