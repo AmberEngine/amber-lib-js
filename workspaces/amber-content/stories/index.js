a7340f33098e4eaff579b0f1bb2c773138325676
Joel Lanciaux <joel@amberengine.com>
2017-11-21 10:37:52 -0500

    Added whats this, link icon.

--- workspaces/amber-content/stories/index.js
+++ workspaces/amber-content/stories/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-storiesOf('amber-content', module)
-  .add('renders amber-content', () => {
+import Icon from '../src/components/Icon';
+import IconListing from '../src/components/Icon/IconsListing';
+const availableIcons = Object.keys(IconListing);
 
+storiesOf('amber-content', module)
+  .add('renders all icons', () => {
+    const iconItems = availableIcons.map(icon => {
+      return (
+        <tr key={icon}>
+          <td><pre>{icon}</pre></td>
+          <td><Icon icon={icon} /></td>
+        </tr>
+      );
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Icon</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          {iconItems}
+        </tbody>
+      </table>
+    );
   });
