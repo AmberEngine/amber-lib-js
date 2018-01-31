ab7c495ac269ea4924f85cd586dbb3f8c4269394
Joel Lanciaux <joel@amberengine.com>
2018-01-31 10:30:05 -0500

    Updated index.

--- workspaces/amber-payments/src/components/index.js
+++ workspaces/amber-payments/src/components/index.js
@@ -1,4 +1,6 @@
 export CreditCardForm from './CreditCardForm';
+export CreditCardFormField from './CreditCardFormField';
+export CreditCardIcon from './CreditCardIcon';
 
 import * as self from './index';
 export default self;
